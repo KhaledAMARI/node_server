318470184e9dce2e37f39219a03afd7bb771a6a7
Khal3dAMARI <khaled.amari@novhadrim.com>
2021-12-11 12:42:01 +0100

    Fix Auth middleware

--- Middlewares/auth.js
+++ Middlewares/auth.js
@@ -1,19 +1,19 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
-const authentificationMiddleware = (req, res, next) => {
+const authentificationMiddleware = async (req, res, next) => {
   const authHeader = req.headers.authorization;
   if (!authHeader || !authHeader.startsWith('Bearer ')) {
     return res.status(401).json({ error: 'You are not authorized to get in'});
   };
   try {
     const token = authHeader.split(' ').pop();
-    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    const decodedToken = await jwt.verify(token, process.env.JWT_SECRET);
     const { id, name } = decodedToken;
     req.user = { id, name };
     next();
   } catch (error) {
-    console.log(error);
+    throw error;
   };
 };
 
