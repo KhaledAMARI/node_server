af665f2905805c03c41051cbfd47b1a52542a971
Khal3dAMARI <khaled.amari@novhadrim.com>
2021-12-18 11:35:53 +0100

    Cleanup User Service and Add email validator

--- Models/users.js
+++ Models/users.js
@@ -1,37 +1,43 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const mongooseSchema = mongoose.Schema;
 
 const schemaOptions = {
   timestamps: true,
   toJSON: {
-    virtuals: true
-  }
+    virtuals: true,
+  },
 };
 
-const UserSchema = new mongooseSchema({
-  name: {
-    type: String,
-    required: [true, 'Please provide a name'],
-    default: ''
+const UserSchema = new mongooseSchema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide a name"],
+      default: "",
+    },
+    email: {
+      type: String,
+      lowercase: true,
+      unique: true,
+      trim: true,
+      // match:  [/.+\@.+\..+/, "Please fill a valid email address."]
+      match: [
+        /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        "Please fill a valid email address.",
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, "Please provide a password"],
+    },
+    isConfirmed: {
+      type: Boolean,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-    lowercase: true,
-    unique: true,
-    trim: true,
-    match:  [/.+\@.+\..+/, "Please fill a valid email address."]
-  },
-  password: {
-    type: String,
-    required: [true, 'Please provide a password']
-  },
-  isConfirmed: {
-    type: Boolean,
-    default: false
-  }
-}, schemaOptions);
+  schemaOptions
+);
 
-const userModel = mongoose.model('Users', UserSchema);
+const userModel = mongoose.model("Users", UserSchema);
 
 module.exports = userModel;
-
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const { register, confirmMail, login, userHome } = require('../Controllers/user');
+const { userHome } = require('../Controllers/user');
 const authentificationMiddleware = require('../Middlewares/auth');
 
 router.route('/').get(authentificationMiddleware, userHome);
-router.route('/register').post(register);
-router.route('/confirm_email').post(confirmMail);
-router.route('/login').post(login);
+
 
 module.exports = router;
